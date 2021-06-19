ec93741b4c8444049d0b981755e9d667bcebe5bf
sadainer <sadainer@hotmail.com>
2021-06-19 15:22:37 -0500

    Update Main.java

--- src/main/java/dad/maven/hola/Main.java
+++ src/main/java/dad/maven/hola/Main.java
@@ -3,8 +3,7 @@ import org.apache.commons.lang3.StringUtils;
 
 public class Main {
 	public static void main(String[] args) {
-		String greeting = StringUtils.reverse("Hola Maven!");
-		
+		String greeting = StringUtils.reverse("Hola Mundo Maven!");
 		System.out.println(greeting);
 		
 	}
